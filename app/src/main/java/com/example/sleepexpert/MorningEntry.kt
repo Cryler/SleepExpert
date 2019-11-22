e3b42575f00168f875690887216c2468a7e2bb71
Cryler <luca.leuenberger@bluewin.ch>
2019-11-22 17:57:25 +0100

    update

--- app/src/main/java/com/example/sleepexpert/MorningEntry.kt
+++ app/src/main/java/com/example/sleepexpert/MorningEntry.kt
@@ -3,7 +3,11 @@ package com.example.sleepexpert
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MorningEntry : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,24 @@ class MorningEntry : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
         }
 
+        val date = Calendar.getInstance().time
+        val formatter = SimpleDateFormat.getDateTimeInstance() //or use getDateInstance()
+        val formattedDate = formatter.format(date)
+
+        val labelDate = findViewById<TextView>(R.id.labelDate)
+        labelDate.setText(formattedDate)
 
     }
+
+    fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
+        val formatter = SimpleDateFormat(format, locale)
+        return formatter.format(this)
+    }
+
+    fun getCurrentDateTime(): Date {
+        return Calendar.getInstance().time
+    }
+
+
+
 }
